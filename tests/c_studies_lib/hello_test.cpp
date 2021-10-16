a7aac8cdfec072c9c40a7f4209971eac6df4f223
Bruno Araujo <araujo.contato@gmail.com>
2021-10-16 15:29:01 -0300

    fix: ASSERT_EQ don't work properly with the hello() function

--- tests/c_studies_lib/hello_test.cpp
+++ tests/c_studies_lib/hello_test.cpp
@@ -5,9 +5,9 @@ extern "C" {
 }
 
 TEST(hello_test,should_return_default_message_when_hello_is_called){
-	ASSERT_EQ(hello_world(), "hello world!");
+	ASSERT_STREQ(hello_world(), "hello world!");
 }
 
 TEST(hello_test,should_return_message_with_the_input_when_hello_is_called){
-	ASSERT_EQ(hello("Pézão"), "hello Pézão!");
+	ASSERT_STREQ(hello("Pézão"), "hello Pézão!");
 }
